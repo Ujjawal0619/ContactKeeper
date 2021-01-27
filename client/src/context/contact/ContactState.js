63b0065e5a80014d044fe31a52ed50d56747c2c6
Ujjawal kumar <ujjawal.kumar0619@gmail.com>
2021-01-27 19:13:57 +0530

    contact Context structure added and ContactState state created with dummy data

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,70 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from "../types";
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "Ujjwal Kumar",
+        email: "ujjwal@gmail.com",
+        phone: "111-111-1111",
+        type: "personal",
+      },
+      {
+        id: 2,
+        name: "Sudhir Kumar",
+        email: "Sudhir@gmail.com",
+        phone: "111-111-1111",
+        type: "personal",
+      },
+      {
+        id: 3,
+        name: "Ravi Kumar",
+        email: "Ravi@gmail.com",
+        phone: "333-111-1111",
+        type: "personal",
+      },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Add Contact
+
+  // Delete Conact
+
+  // Set Current Contact
+
+  // Clear Current Contact
+
+  // Update Contact
+
+  // Filter Contacts
+
+  // Clear Filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
