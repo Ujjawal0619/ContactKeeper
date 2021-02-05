19469ac4b773c8f3ac3de52c9eed9be15e9d146e
Ujjawal kumar <ujjawal.kumar0619@gmail.com>
2021-02-05 13:08:02 +0530

    Alert Component and Context is added with validation in Register route

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,60 +1,69 @@
-import React, { useState } from "react";
+import { useContext, useState } from 'react';
+import AlertContext from '../../context/alert/alertContext';
 
 const Register = () => {
+  const alertContext = useContext(AlertContext);
+  const { setAlert } = alertContext;
   const [user, setUser] = useState({
-    name: "",
-    emain: "",
-    password: "",
-    password2: "",
+    name: '',
+    emain: '',
+    password: '',
+    password2: '',
   });
 
   const { name, email, password, password2 } = user;
 
   const onChange = (e) => {
-    setUser({ ...user, [e.targer.name]: e.target.value });
+    setUser({ ...user, [e.target.name]: e.target.value });
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("User Registered");
+    if (name === '' || email === '' || password === '') {
+      setAlert(' Please enter all fields', 'danger');
+    } else if (password !== password2) {
+      setAlert(' Password do nto match', 'danger');
+    } else {
+      console.log('User Registered');
+    }
   };
 
   return (
-    <div className="form-container">
+    <div className='form-container'>
       <h1>
-        Account <span className="text-primary">Register</span>
+        Account <span className='text-primary'>Register</span>
       </h1>
       <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" value={name} onChange={onChange} />
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
+          <input type='text' name='name' value={name} onChange={onChange} />
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Email Address</label>
-          <input type="email" name="email" value={email} onChange={onChange} />
+        <div className='form-group'>
+          <label htmlFor='email'>Email Address</label>
+          <input type='email' name='email' value={email} onChange={onChange} />
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
           <input
-            type="password"
-            name="password"
+            type='password'
+            name='password'
             value={password}
             onChange={onChange}
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="password2">Confirm Password</label>
+        <div className='form-group'>
+          <label htmlFor='password2'>Confirm Password</label>
           <input
-            type="password"
-            name="password2"
+            type='password'
+            name='password2'
             value={password2}
             onChange={onChange}
           />
         </div>
         <input
-          type="submit"
-          value="Register"
-          className="btn btn-primary btn-block"
+          type='submit'
+          value='Register'
+          className='btn btn-primary btn-block'
         />
       </form>
     </div>
---
+++ client/src/components/layout/Alerts.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+
+const Alerts = () => {
+  const alertContext = useContext(AlertContext);
+  console.log(alertContext.alerts);
+  return alertContext.alerts.map((alert) => (
+    <div key={alert.id} className={`alert alert-${alert.type}`}>
+      <i className='fas fa-info-circle' />
+      {alert.msg}
+    </div>
+  ));
+};
+
+export default Alerts;
