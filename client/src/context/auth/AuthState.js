ea4af0ce081c377014d6be8298099a0af4ae5191
Ujjawal kumar <ujjawal.kumar0619@gmail.com>
2021-02-09 22:52:02 +0530

    login & logout functionality completed

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -50,7 +50,7 @@ const AuthState = (props) => {
       },
     };
     try {
-      const res = await axios.post('api/users', formData, config);
+      const res = await axios.post('/api/users', formData, config);
 
       dispatch({
         type: REGISTER_SUCCESS,
@@ -65,7 +65,28 @@ const AuthState = (props) => {
     }
   };
   // Login User
-  const login = () => console.log('login');
+  const login = async (formData) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+      loadUser();
+    } catch (err) {
+      console.log(err.response.data);
+
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
+  };
 
   // Logout
   const logout = () => console.log('logout');
