6c912e32d29d0c38b69ccbcbddd5684d48370774
Ujjawal kumar <ujjawal.kumar0619@gmail.com>
2021-02-24 16:28:34 +0530

    typo fixed

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.use('/api/contacts', require('./routes/contacts')); // postman- {POST} http:
 // Serve static(react) assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
+  app.use(express.static('client/build'));
+
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
