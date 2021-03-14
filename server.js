66aae742e8335d52ee604bdd4618ad99a6a8b26c
Ujjawal kumar <ujjawal.kumar0619@gmail.com>
2021-03-14 23:46:59 +0530

    revised the mailing time to once at 00:00:00

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
   // SMS texting scheduler
-  schedule.scheduleJob('*/5 * * * *', () => {
+  schedule.scheduleJob('0 0 * * *', () => {
     console.log(`scheduler runs @ ${new Date().toLocaleString()}`);
     messanger();
   });
